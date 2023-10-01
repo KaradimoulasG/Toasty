9ab2b510b647892ca3375da63486e9f36f69dc09
George <karadimoulas@gmail.com>
2023-10-01 22:57:27 +0300

    Added Coil dependency for new features. Will work on them over the next few days

--- toasty/build.gradle.kts
+++ toasty/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
 
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
 }
 
 publishing {
